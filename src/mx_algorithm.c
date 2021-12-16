934dd26003169e968eec121153e02e2fe4d2b033
fstaryk <fstaryk@student.ucode.world>
2021-12-16 20:38:32 +0200

    started adding reversability

--- src/mx_algorithm.c
+++ src/mx_algorithm.c
@@ -68,6 +68,9 @@ int check_optimal_route(t_route checked, int **matrix, t_ind_len *ind_len, int n
             if (ind_s == ind_len[count2].start && ind_d == ind_len[count2].dest) {
                 sub_path = ind_len[count2].len;
             }
+            else if (ind_d == ind_len[count2].start && ind_s == ind_len[count2].dest) {
+                sub_path = ind_len[count2].len;
+            }
         }
         sum += sub_path;
     }
@@ -91,7 +94,9 @@ int get_index_from_route_struct(int start, int dest, int path_num, t_route *rout
         if (start == route[i].start && dest == route[i].dest) {
             return i;
         }
-        
+        if(dest == route[i].start && start == route[i].dest){
+            return i;
+        }
     }
     return -1;
 }
@@ -101,6 +106,7 @@ int get_num_index_in_arr(int num, int *arr, int arr_size) {
         if (num == arr[i]) {
             return i;
         }
+        
     }
     return -1;
 }
@@ -144,8 +150,10 @@ post_algorithm_processing(t_route *struct_route, int side_size, int **matrix, t_
     for (int count1 = 0; count1 < path_num; count1++) {
 
         // WHILE PATH IS NOT OPTIMAL OPTIMISATION CONTINOUS
+        //print_single_route_struct(struct_route[count1]);
         while (check_optimal_route(struct_route[count1], matrix, ind_len, namesLen) != 1) {
             // Going through all the destinations
+            fprintf(stderr, "\tinfinite loop check\n");
             for (int count2 = 0; count2 < get_index_route(struct_route[count1].list) - 2;
                  count2++) { // Probably should be changed to "-1", instead of "-2"
                 int temp_start = struct_route[count1].list[count2];
